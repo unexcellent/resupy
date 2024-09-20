22c2e00189cb8d73f35afefe72e02dff9c866c88
unexcellent <>
2024-09-20 22:20:32 +0200

    feat: add award schema

---
+++ resupy/resume_data/award.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Award:
+    """An award you have received throughout your professional career."""
+
+    title: Optional[str]
+    """Title of the award."""
+
+    date: Optional[str]
+    """The date you were awarded like'2014-06-29' or '2023-04' or 'Oct
+    2022'."""
+
+    awarder: Optional[str]
+    """Organization handing out the award."""
+
+    summary: Optional[str]
+    """Short summary of why it was awarded."""
