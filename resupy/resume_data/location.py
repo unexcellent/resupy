f25a318890f383add3d2df96fa7a028a3f7df54b
unexcellent <>
2024-09-20 21:24:51 +0200

    refactor!: focus on important fields in location

--- resupy/resume_data/location.py
+++ resupy/resume_data/location.py
@@ -6,20 +6,14 @@ from typing import Optional
 class Location:
     """Your location used for sending mail."""
 
-    address: Optional[str]
+    address: str
     """Your address like '1234 Glücklichkeitsstraße'."""
 
-    postal_code: Optional[str]
+    postal_code: str
     """Your postal code like '012345'."""
 
-    city: Optional[str]
+    city: str
     """The name of your city."""
 
-    country_code: Optional[str]
-    """The country code as per ISO-3166-1 ALPHA-2, e.g. US, AU, IN."""
-
     region: Optional[str]
-    """The general region where you live.
-
-    Can be a US state, or a province, for instance.
-    """
+    """The general region where you live like 'Australia' or 'New Jersey'."""
