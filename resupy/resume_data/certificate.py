9ef9da151904e431dd3d7a0e992dc5f42aa9634e
unexcellent <>
2024-09-20 22:22:44 +0200

    feat: add certificate schema

---
+++ resupy/resume_data/certificate.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Certificate:
+    """A certificate you have received throughout your professional career."""
+
+    name: Optional[str]
+    """Name of the certificate."""
+
+    date: Optional[str]
+    """The date you were certified like'2014-06-29' or '2023-04' or 'Oct
+    2022'."""
+
+    url: Optional[str]
+    """URL to the issuer or certificate."""
+
+    issuer: Optional[str]
+    """Organization handing out the certificate."""
