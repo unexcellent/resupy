78842a25f6555c1efe89ca2c53fd0ec44821d516
unexcellent <>
2024-09-21 14:19:40 +0200

    feat: add root resume data schema

---
+++ resupy/resume_data/resume_data.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from .award import Award
+from .basics import Basics
+from .certificate import Certificate
+from .education import Education
+from .interest import Interest
+from .language import Language
+from .project import Project
+from .publication import Publication
+from .reference import Reference
+from .skill import Skill
+from .volunteer import Volunteer
+from .work import Work
+
+
+@dataclass
+class ResumeData:
+    """Root data container of resume data."""
+
+    basics: Optional[Basics]
+    """Basic information about yourself."""
+
+    work: Optional[list[Work]]
+    """Your job positions."""
+
+    volunteer: Optional[list[Volunteer]]
+    """Your volunteering experience."""
+
+    education: Optional[list[Education]]
+    """Your educational history."""
+
+    awards: Optional[list[Award]]
+    """Awards you have received."""
+
+    certificates: Optional[list[Certificate]]
+    """Certificates you have received."""
+
+    publications: Optional[list[Publication]]
+    """Any publications you have made."""
+
+    skills: Optional[list[Skill]]
+    """Your professional skills."""
+
+    languages: Optional[list[Language]]
+    """The languages you know."""
+
+    interests: Optional[list[Interest]]
+    """Your private interests."""
+
+    references: Optional[list[Reference]]
+    """Any references you want to list."""
+
+    projects: Optional[list[Project]]
+    """Specific career projects."""
