0650e047ed0633174dc4f71c31b3bf981196264b
unexcellent <>
2024-09-21 16:14:26 +0200

    feat: from_dict() with nested layers

--- resupy/resume_data/_jsonresume_wrapper.py
+++ resupy/resume_data/_jsonresume_wrapper.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, fields
+from typing import Any
 
 import humps  # type: ignore
 
@@ -8,20 +9,47 @@ class _JsonresumeWrapper:
     """Parent to all classes directly resembling jsonresume data."""
 
     @classmethod
-    def field_names(cls) -> list[str]:
-        """Return the field names associated with this class."""
-        return [field.name for field in fields(cls)]
+    def fields(cls) -> dict[str, type]:
+        """Return the field name and typ associated with this class."""
+        return {field.name: field.type for field in fields(cls)}
 
     @classmethod
-    def from_dict(cls, dict_data: dict) -> "_JsonresumeWrapper":
+    def from_dict(cls, source_dict: dict) -> "_JsonresumeWrapper":
         """Construct this class from a jsonresume dict."""
 
-        sanitized_dict_data = {}
-        for field_name in cls.field_names():
+        sanitized_dict_data: dict[str, Any] = {}
+        for field_name, field_type in cls.fields().items():
+
+            # All fields in the dataclasses are typing.Optional[T]. Only the type T is of interest
+            # and is extracted here.
+            field_class = field_type.__args__[0]  # type: ignore
+
             # Since the dataclass fields are in snake case and the jsonresume fields are in camel
             # case, the field names have to be camelized first.
             source_dict_key = humps.camelize(field_name)
 
-            sanitized_dict_data[field_name] = dict_data.get(source_dict_key)
+            source_dict_does_not_contain_field = (
+                source_dict_key not in source_dict or source_dict[source_dict_key] is None
+            )
+            if source_dict_does_not_contain_field:
+                sanitized_dict_data[field_name] = None
+                continue
+
+            # If the inner class of a field has the from_dict() method, it should be constructed
+            # via from_dict(). If it does not, the default constructor should be used.
+            if _class_has_from_dict_method(field_class):
+                sanitized_dict_data[field_name] = field_class.from_dict(source_dict[source_dict_key])
+            else:
+                sanitized_dict_data[field_name] = field_class(source_dict[source_dict_key])
 
         return cls(**sanitized_dict_data)
+
+
+def _class_has_from_dict_method(cls: type) -> bool:
+    """Return True if a class has a from_dict() method."""
+    try:
+        cls.from_dict({})  # type: ignore
+    except AttributeError:
+        return False
+    else:
+        return True
--- tests/resume_data/test_jsonresume_wrapper.py
+++ tests/resume_data/test_jsonresume_wrapper.py
@@ -1,22 +1,22 @@
 import pytest
 
-from resupy.resume_data import Award, Language, Location, Skill
+from resupy.resume_data import Award, Basics, Language, Location, Skill
 
 
 def test_from_dict__simple():
-    dict_data = {
+    source_dict = {
         "language": "English",
         "fluency": "Native",
     }
 
-    assert Language.from_dict(dict_data) == Language(
+    assert Language.from_dict(source_dict) == Language(
         language="English",
         fluency="Native",
     )
 
 
 def test_from_dict__unexpected_field():
-    dict_data = {
+    source_dict = {
         "name": "Microsoft Excel",
         "level": "I can run Doom on it.",
         "keywords": [
@@ -26,7 +26,7 @@ def test_from_dict__unexpected_field():
         "UNEXPECTED_ARGUMENT": "some_value",
     }
 
-    assert Skill.from_dict(dict_data) == Skill(
+    assert Skill.from_dict(source_dict) == Skill(
         name="Microsoft Excel",
         level="I can run Doom on it.",
         keywords=[
@@ -37,14 +37,14 @@ def test_from_dict__unexpected_field():
 
 
 def test_from_dict__missing_field():
-    dict_data = {
+    source_dict = {
         "title": "Milwaukee Hot Dog Eating Champion",
         "date": "2012-06-14",
         "awarder": "Major League Gluttony",
         # summary field is missing
     }
 
-    assert Award.from_dict(dict_data) == Award(
+    assert Award.from_dict(source_dict) == Award(
         title="Milwaukee Hot Dog Eating Champion",
         date="2012-06-14",
         awarder="Major League Gluttony",
@@ -53,14 +53,32 @@ def test_from_dict__missing_field():
 
 
 def test_from_dict__camel_case_to_snake_case():
-    dict_data = {
+    source_dict = {
         "postalCode": "1235",
         "countryCode": "FR",
     }
 
-    assert Location.from_dict(dict_data) == Location(
+    assert Location.from_dict(source_dict) == Location(
         postal_code="1235",
         country_code="FR",
+    )
+
+
+def test_from_dict__nested_layers():
+    source_dict = {
+        "name": "Bilbo Baggins",
+        "location": {
+            "city": "Bag End",
+            "region": "Hobbiton",
+        },
+    }
+
+    assert Basics.from_dict(source_dict) == Basics(
+        name="Bilbo Baggins",
+        location=Location(
+            city="Bag End",
+            region="Hobbiton",
+        ),
     )
 
 
