df51578237a4e23322894d7c0eaf0048a63df2d3
unexcellent <>
2024-09-21 14:11:52 +0200

    feat: add project schema

---
+++ resupy/resume_data/project.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Project:
+    """Specific career projects."""
+
+    name: Optional[str]
+    """Name of the project."""
+
+    description: Optional[str]
+    """Short summary of the project.."""
+
+    highlights: Optional[list[str]]
+    """Accomplishments you had."""
+
+    keywords: Optional[list[str]]
+    """Special elements involved."""
+
+    start_date: Optional[str]
+    """The date you have started like '2014-06-29' or '2023-04' or 'Oct
+    2022'."""
+
+    end_date: Optional[str]
+    """The date the project was ended like '2014-06-29' or '2023-04' or 'Oct
+    2022' or 'Now' if it is still ongoing."""
+
+    url: Optional[str]
+    """URL to the project."""
+
+    roles: Optional[list[str]]
+    """Specify your roles on this project."""
+
+    entity: Optional[str]
+    """A relevant entity or organization."""
+
+    type: Optional[str]
+    """Like 'volunteering', 'presentation', 'talk', 'application',
+    'conference'."""
