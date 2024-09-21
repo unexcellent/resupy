d927294d1018a0f28ea578671081d3e85ab39d76
unexcellent <>
2024-09-21 14:50:59 +0200

    test: from_dict() add test for unexpected field

--- tests/resume_data/test_jsonresume_wrapper.py
+++ tests/resume_data/test_jsonresume_wrapper.py
@@ -1,6 +1,7 @@
 import pytest
 
 from resupy.resume_data.language import Language
+from resupy.resume_data.skill import Skill
 
 
 def test_from_dict__single_layer_all_fields_present():
@@ -13,6 +14,21 @@ def test_from_dict__single_layer_all_fields_present():
         language="English",
         fluency="Native",
     )
+
+
+def test_from_dict__single_layer_unexpected_field():
+    dict_data = {
+        "name": "Microsoft Excel",
+        "level": "I can run Doom on it.",
+        "keywords": [
+            "Office",
+            "Spreadsheets",
+        ],
+        "UNEXPECTED_ARGUMENT": "some_value",
+    }
+
+    with pytest.raises(TypeError):
+        Skill.from_dict(dict_data)
 
 
 if __name__ == "__main__":
