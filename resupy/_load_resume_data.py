c225c73f757f7f4c2f82e9c3476e22c6c1eaa641
unexcellent <>
2024-09-21 19:34:06 +0200

    feat: load_resume_data() json with comments loading

--- resupy/_load_resume_data.py
+++ resupy/_load_resume_data.py
@@ -1,11 +1,12 @@
-import json
 from pathlib import Path
+
+import json5
 
 
 def load_resume_data(resume_data_path: Path) -> dict:
     """Load the resume data from a file."""
 
     with resume_data_path.open() as resume_data_file:
-        resume_data = json.load(resume_data_file)
+        resume_data = json5.load(resume_data_file)
 
     return resume_data
--- tests/test_load_resume_data.py
+++ tests/test_load_resume_data.py
@@ -22,5 +22,25 @@ def test_json(tmp_path):
     }
 
 
+def test_json_with_comments(tmp_path):
+    file_content = """
+    {
+        // This is a comment that normal JSON does not like
+        "basics": {
+            "name": "John Doe",
+            "label": "The most average person you will ever meet",
+        }
+    }"""
+    path = tmp_path / "resume_data.json"
+
+    with path.open("w") as file:
+        file.write(file_content)
+
+    actual = load_resume_data(path)
+    assert actual == {
+        "basics": {"name": "John Doe", "label": "The most average person you will ever meet"}
+    }
+
+
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
